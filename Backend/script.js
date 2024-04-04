16d706551e77bfd711f885403aa2cbb2a6f3f512
Leeja Shrestha <np03cs4a220084@heraldcollege.edu.np>
2024-04-04 13:44:11 +0545

    script changes

--- Backend/script.js
+++ Backend/script.js
@@ -1,4 +1,6 @@
 fetch("http://localhost:3500/register", {method:"POST", body:{
     name: "leeja",
-    pwd: "somslekjlkasj"
+    pwd: "somslekjlkasj",
+    phn:"234",
+    eml:"asdsad"
 }, headers:{"Content-Type": "application/json"}}).then(res=>res.json()).then(data=>console.log(data));
